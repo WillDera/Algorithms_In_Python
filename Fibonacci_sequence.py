d0fac71e81c93ca83e20768e56652f5885da92f7
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-05 19:58:56 +0100

    Update Fibonacci_sequence.py
    Memoization is a technique in which you store the results of computational tasks when
    they are completed so that when you need them again, you can look them up instead
    of needing to compute them a second (or millionth) time.
    
    More like State in Javascript

--- Fibonacci_sequence.py
+++ Fibonacci_sequence.py
@@ -1,8 +1,15 @@
+# Introducing memotization
+from typing import Dict
+memo: Dict[int, int] = {0: 0, 1: 1}  # Our base cases
+
+
 def fib(n: int) -> int:
-    if n < 2:  # base case
-        return n
-    return fib(n - 2) + fib(n - 1)  # recursive case
+    if n not in memo:
+        memo[n] = fib(n - 1) + fib(n - 2)  # memotization
+    return memo[n]
+    # return fib(n - 2) + fib(n - 1)  # recursive case
 
 
 if __name__ == "__main__":
     print(fib(5))
+    print(fib(50))
