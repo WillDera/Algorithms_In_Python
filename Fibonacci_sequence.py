4e5c2d687397ac5b50256d30f35467b2290daf6b
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-05 20:20:28 +0100

    Update Fibonacci_sequence.py
    Converting our function into a Python Generator.

--- Fibonacci_sequence.py
+++ Fibonacci_sequence.py
@@ -1,15 +1,33 @@
-# Introducing memotization
-from typing import Dict
-memo: Dict[int, int] = {0: 0, 1: 1}  # Our base cases
+# from functools import lru_cache
 
 
-def fib(n: int) -> int:
-    if n not in memo:
-        memo[n] = fib(n - 1) + fib(n - 2)  # memotization
-    return memo[n]
-    # return fib(n - 2) + fib(n - 1)  # recursive case
+# @lru_cache(maxsize=None)
+# def fib4(n: int) -> int:  # same definition as fib2()
+#     if n < 2:  # base case
+#         return n
+#     return fib4(n - 2) + fib4(n - 1)  # recursive case
+
+
+from typing import Generator
+
+
+def fib6(n: int) -> Generator[int, None, None]:
+    yield 0  # special case
+
+    if n > 0:
+        yield 1  # special case
+    last: int = 0  # initially set to fib(0)
+    next: int = 1  # initially set to fib(1)
+
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next  # main generation step
 
 
 if __name__ == "__main__":
-    print(fib(5))
-    print(fib(50))
+    for i in fib6(50):
+        print(i)
+
+# if __name__ == "__main__":
+#     print(fib4(5))
+#     print(fib4(50))
