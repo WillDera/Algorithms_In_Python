f9166be5a4fc2d2d40f31822f6ef3beba214cd1b
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-05 19:47:59 +0100

    Update Fibonacci_sequence.py

--- Fibonacci_sequence.py
+++ Fibonacci_sequence.py
@@ -1,2 +1,8 @@
 def fib(n: int) -> int:
-    return fib(n - 1) + fib(n - 2)
+    if n < 2:  # base case
+        return n
+    return fib(n - 2) + fib(n - 1)  # recursive case
+
+
+if __name__ == "__main__":
+    print(fib(5))
