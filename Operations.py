4b9f3ef7eb920a494845935c3f9ffb0fa3421cf9
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-09 20:08:13 +0100

    Update Operations.py
    Done

--- Operations.py
+++ Operations.py
@@ -1,8 +1,7 @@
-# L, denotes a given list
+# L, denotes a given list eg [1, 2, 4], ['hello', 'hide' 'a', 'my name']
 # p, denotes the position in L
 
-test_list = [1, 2, 3, 4]
-
+# p must be an integer eg. 1, 2, 10, 3, 4 or 5
 
 # FIRST(L) returns the first element in L
 def FIRST(L: list):
@@ -20,20 +19,23 @@ def RETRIEVE(p: int, L: list):
     return ("%s is element at position %s in the provided List" % (L[p], p))
 
 
+# NEXT(p, L) returns the next position after the provided p in L
 def NEXT(p: int, L: list):
-    if len(L) > p:
-        temp_position = p + 1
+    temp_position = p + 1
+    if p < 0:
+        return IndexError("LIST position cannot be negative")
+    elif len(L) > p:
         return ("Next position is %s" % temp_position)
     else:
-        return ("Provided position not in range of %s" % L)
+        return IndexError("Provided position not in range of %s which has a length of %s" % (L, len(L)))
 
 
+# PREVIOUS(p, L) returns the previous position before the provided p in L
 def PREVIOUS(p: int, L: list):
     temp_position = (p) - 1
-    if temp_position <= 0:
-        return ("0 is the first position of a List")
+    if temp_position < 0:
+        return IndexError("LIST position cannot be negative")
+    elif p == 0:
+        return ("0 is the first index of a LIST")
     else:
-        return ("Provided positon of - %s is out of range of list - %s" % (p, L))
-
-
-print(NEXT(6, [3, 4, 5, 6, 6, 7]))
+        return ("Previous position is %s" % temp_position)
