fa6a9f3ed234cffb613ea46aa564411ee8fd2545
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-20 06:27:14 +0100

    Update Operations_v2.py
    - create locate(x,L) operation

--- Operations_v2.py
+++ Operations_v2.py
@@ -10,9 +10,31 @@ temp_list = [1, "hello", "4", 6, 10]
 # if L does not have position p, print an error message
 def insert(x, p: int, L: list):
     if p < 0:
-        return IndexError("LIST position cannot be negative")
+        return AttributeError("LIST position cannot be negative")
     elif p <= len(L):
         L[p] = x
         return ("Element %s successfully inserted at index %s in the list" % (x, p))
     else:
         return IndexError("Requested position not in range of %s" % L)
+
+
+# return the position of x in L; if x appears more than once return the position of the first appearance;
+# if x does not exist in L print an error message and return return END(L)
+def locate(x, L: list):
+    if x in L:
+        p = L.index(x)
+        return ("Element found at index %s in %s" % (p, L))
+    elif x not in L:
+        end = END(L)
+        return ("Element not found in the provided LIST \n%i" % end)
+    else:
+        return ("Something went wrong")
+
+
+# return the position following the last position on L
+def END(L):
+    p = len(L) + 1
+    return p
+
+
+print(locate(16, temp_list))
