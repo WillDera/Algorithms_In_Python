387caa023861d0e3d0c2eef117761fadc48a9d23
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-20 06:44:56 +0100

    Update Operations_v2.py
    - randomly generating a list with insert(x,p,L) when the program is started

--- Operations_v2.py
+++ Operations_v2.py
@@ -1,9 +1,8 @@
+import random
 # L, denotes a given list eg [1, 2, 4], ['hello', 'hide' 'a', 'my name']
 # p, denotes the position in L
 # x, denotes an element
-
 # p must be an integer eg. 1, 2, 10, 3, 4 or 5
-temp_list = [1, "hello", "4", 6, 10]
 
 
 # insert x at position p on L. if p=END(L) insert at the end of L;
@@ -12,7 +11,7 @@ def insert(x, p: int, L: list):
     if p < 0:
         return AttributeError("LIST position cannot be negative")
     elif p <= len(L):
-        L[p] = x
+        L.insert(p, x)
         return ("Element %s successfully inserted at index %s in the list" % (x, p))
     else:
         return IndexError("Requested position not in range of %s" % L)
@@ -31,10 +30,20 @@ def locate(x, L: list):
         return ("Something went wrong")
 
 
+# return the element at position p on L; if L does not have postion p or
+# if p=END(L), then print an error message
+
 # return the position following the last position on L
 def END(L):
     p = len(L) + 1
     return p
 
 
-print(locate(16, temp_list))
+# randomly generated list using insert(x,p,L)
+random_List = []
+for i in range(0, 6):
+    # for m in range(0, 5):
+    x = random.randint(1, 50)
+    insert(x, i, random_List)
+
+print(random_List)
