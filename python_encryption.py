84c50552b596d0b80ec48dea53022266c5d8e0e1
Godswill <30760648+WillDera@users.noreply.github.com>
2020-07-25 22:23:50 +0100

    Update python_encryption.py
    created the encryption functionality

---
+++ python_encryption.py
@@ -0,0 +1,25 @@
+# Here we encrypt a str using a one-time pad, A one-time pad is a way of encrypting a piece of data by combining it with meaningless
+# random dummy data in such a way that the original cannot be reconstituted without
+# access to both the product and the dummy data
+
+
+from secrets import token_bytes
+from typing import Tuple
+
+
+# This function creates an int filled with length random bytes.
+def random_key(length: int) -> int:
+    # generate length random bytes
+    tb: bytes = token_bytes(length)
+
+    # convert those bytes into a bit string and return it
+    return int.from_bytes(tb, "big")
+
+
+# This function encodes the str passeed into it and returns a key pair (dummy and encrypted)
+def encrypt(original: str) -> Tuple[int, int]:
+    original_bytes: bytes = original.encode()
+    dummy: int = random_key(len(original_bytes))
+    original_key: int = int.from_bytes(original_bytes, "big")
+    encrypted: int = original_key ^ dummy   # XOR (exlusive or)
+    return dummy, encrypted
