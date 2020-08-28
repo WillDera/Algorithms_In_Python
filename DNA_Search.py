05b4f8c8cfdc1d778bd1f9a80f22ff54f1363b71
Godswill <30760648+WillDera@users.noreply.github.com>
2020-08-28 21:17:16 +0100

    Update DNA_Search.py

--- DNA_Search.py
+++ DNA_Search.py
@@ -9,3 +9,24 @@ Nucleotide: IntEnum = IntEnum('Nuecleotide', ('A', 'C', 'G', 'T'))
 
 Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]  # type alias for codons
 Gene = List[Codon]  # type alias for genes
+
+# Our Imaginary Gene str
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+# Function to convert our str to a Gene
+
+
+def string_to_Gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s):  # dont run off end
+            return gene
+        # initialize codon out of three nucleotides
+        codon: Codon = (Nucleotide[s[i]],
+                        Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon)  # add codon to gene
+        return gene
+
+
+# converting gene_str to Gene
+my_gene: Gene = string_to_Gene(gene_str)
